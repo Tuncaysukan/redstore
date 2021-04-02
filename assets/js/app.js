169e4ad531bb2579f5a8b84d3d04f12eb0355658
tuncay <tuncay@mekayazilim.com>
2021-04-02 18:01:48 +0300

    s
    s

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+var menuItems=document.getElementById('menuItems');
+
+
+function menutoggle(){
+    if(menuItems.style.maxHeight=='0px'){
+        menuItems.style.maxHeight='200px';
+       
+    }
+    else{
+        menuItems.style.maxHeight='0px';
+        menuItems.style.maxHeight='0px';
+        
+    }
+}
+var product=document.getElementById('product-img');
+var smallImg=document.getElementsByClassName('small-img');
+
+smallImg[0].onclick=function(){
+    product.src=smallImg[0].src;
+}
+smallImg[1].onclick=function(){
+    product.src=smallImg[1].src;
+}
+smallImg[2].onclick=function(){
+    product.src=smallImg[2].src;
+}
+smallImg[3].onclick=function(){
+    product.src=smallImg[3].src;
+}
